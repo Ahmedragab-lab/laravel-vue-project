c7a815cc481d2251ed7cd4226d4047c087c4f1ea
wolf <ahmedragabyasin2020@gmail.com>
2022-06-21 00:05:22 +0200

    first vue 2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
+
 require('./bootstrap');
+import Vue from 'vue';
+import App from './components/App.vue';
+// window.Vue = require('vue').default;
+
+
+
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+
+
+const app = new Vue({
+    el: '#app',
+    components: {
+        App
+    }
+});
